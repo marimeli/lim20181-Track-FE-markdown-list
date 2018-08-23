ba24fecc675c1228abef4c24c37c3daad7cdf8fe
marimeli <marimel.casas@gmail.com>
2018-08-23 14:39:44 -0500

    corrige validatePath

--- cli.js
+++ cli.js
@@ -1,31 +1,22 @@
 #!/usr/bin/env node
+/* const path = require('path');
+const fs = require('fs'); */
 const [, , ...args] = process.argv
-console.log(`hello world ${args}`)
+/* console.log(`hello world ${args}`) */
 
-const r = new RegExp('^(?:[a-z]+:)?//', 'i');
-const abs = 'Es una url absoluta';
-const rel = 'Es una ruta relativa';
+const url = new RegExp('^(?:[a-z]+:)?//', 'i');
+const urlAbsolute = 'Es una ruta absoluta';
+const urlRelative = 'Es una ruta relativa';
 
-/* let options = {
-    stats=true,
-    validate=true
-} */
-
-const validatePath = (path) => {
-    // validar si es relativo o absoluto
-    // ver regex 
-    // cuando sea relativo, => hacer que sea absoluto resolve()  o isabsolute()
-    // retorna url absoluta
-    if (r.test(path) === true) {
-        console.log(abs);
-        return abs;
+const validatePath = (root) => {
+    if (url.test(root) === true) {
+        console.log(urlAbsolute);
+        return urlAbsolute;
     }
-    console.log(rel);
-    return rel;
+    console.log(urlRelative);
+    return urlRelative;
 };
-
 validatePath(args);
-
 module.exports = validatePath;
 
 
@@ -33,4 +24,5 @@ const validateFile = () => {
 };
 
 const mdLinks = (path, options) => {
+   /*  const pathFile = validatePath(path.resolve(root)); */
 };
--- test/cli.test.js
+++ test/cli.test.js
@@ -7,15 +7,15 @@ describe('validatePath', () => {
   });
 
   it('Debería validatePath la url http://google.com y mostrar el siguiente texto: Es una url absoluta ', () => {
-    expect(validatePath('http://google.com')).toBe('Es una url absoluta');
+    expect(validatePath('http://google.com')).toBe('Es una ruta absoluta');
   });
 
   it('Debería validatePath la url /directorio/prueba.txt y mostrar el siguiente texto: Es una ruta relativa', () => {
     expect(validatePath('/directorio/prueba.txt')).toBe('Es una ruta relativa');
   }); 
 
-  it('Debería validar la url C:\Users\MelissaCasas\Documents\lim20181-Track-FE-markdown-list-master y mostrar el siguiente texto: Es una ruta relativa', () => {
-    expect(validatePath('np\Users\MelissaCasas\Documents\lim20181-Track-FE-markdown-list-master')).toBe('Es una ruta relativa');
+  it('Debería validar la url  D:\Data\Shapefiles\Soils y mostrar el siguiente texto: Es una ruta relativa', () => {
+    expect(validatePath(' D:\Data\Shapefiles\Soils')).toBe('Es una ruta relativa');
   }); 
  
 });
