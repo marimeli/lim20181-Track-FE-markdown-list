4b63246e07ebf209aed7eae1a2d4ee7f7ff2ff90
marimeli <marimel.casas@gmail.com>
2018-09-06 04:27:31 -0500

    closes#6

--- index.js
+++ index.js
@@ -1,65 +1,67 @@
 const path = require('path');
 const fs = require('fs-extra');
 
-//Funcion que valida si es archivo o directorio
-/* const validateIsFileOrDirectory = (file) => {
-    try {
-        let stats = fs.statSync(file);
-        if (stats.isFile()) return ('file');
-        else if (stats.isDirectory()) return ('directory');
-    } 
-    return urlRelative;
-};  */
+//Función que resuelve una ruta como absoluta
+const validatePath = (root) => {
+    if (path.isAbsolute(root)) {
+        console.log('Es una ruta absoluta');
+        console.log(path.isAbsolute());
+    } else {
+        console.log('Es una ruta relativa convertida a absoluta');
+        console.log(path.resolve());
+    }
+  }
+validatePath('readme.md');
 
-const validateIsFile = (file) => {
+//Función que valida si es un archivo
+const validateIsFile = (file) =>
     fs.lstat(file)
         .then(stat => {
-            /* console.log(stat); */
-            console.log(stat.isDirectory());
-       /*      stat.isDirectory(); //=> true / false */
+            console.log('Archivo ' + stat.isFile());
+            /*   stat.isFile(); //=> true / false */
+        })
+        .catch(err => {
+            console.log(err);
+        });
+validateIsFile('readme.md');
+
+//Función que valida si es un directorio
+const validateIsDirectory = (directory) => {
+    fs.lstat(directory)
+        .then(stat => {
+            console.log('Directorio ' + stat.isDirectory());
+            /*      stat.isDirectory(); //=> true / false */
         })
         .catch(err => {
             console.log(err);
         });
 };
-validateIsFile('C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory');
-
-const validateIsDirectory = (file) =>
-    fs.lstat(file)
-        .then(stat => {
-          /*   console.log(stat); */
-            console.log(stat.isFile());
-          /*   stat.isFile(); //=> true / false */
-        })
-        .catch(err => {
-            console.log(err);
-        });
 validateIsDirectory('C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory');
 
-
 //Función que lee un archivo (falta filtrar que sea md)
-const readContentFile = (path) => {
+const readFileContent = (path) => {
     fs.readFile(path, 'utf8')
         .then(content => {
-            console.log(content);
+            console.log('Contenido File: ' + content);
         })
         .catch(err => {
             console.log(err);
         });
 };
-readContentFile('readme.md');
+readFileContent('readme.md');
 
 //Función que lee un directorio(falta recursividad)
-const readDirectory = (path) => {
+const readDirectoryContent = (path) => {
     fs.readdir(path)
         .then(files => {
+            console.log('Contenido Directory:');
             console.log(files);
         })
         .catch(err => {
             console.log(err);
         });
 };
-readDirectory('C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory');
+readDirectoryContent('C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory');
 
 const mdLinks = (path, options) => {
     return new Promise((resolve, reject) => {
