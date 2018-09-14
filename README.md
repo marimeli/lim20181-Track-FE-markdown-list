250d30a29b95a44f43814eed55c9d29af71b627d
marimeli <marimel.casas@gmail.com>
2018-09-14 16:34:57 -0500

    recursividad

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-This is a read me.
+Reading a file!
+
+Hey! This is a read me.
 
 #### `mdLinks(path, options)`
  ![img_20180807_161238402](https://user-images.githubusercontent.com/32286663/43803949-faa92368-9a5f-11e8-95d8-181de7121d34.jpg)
+
  ##### Argumentos
- - `path`: Ruta absoluta o relativa al archivo markdown. Si la ruta pasada es relativa, debe resolverse como relativa al directorio desde donde se invoca node - _currentworking directory_).
-@@ -157,8 +155,6 @@ manera a través de la consola:
+ - `Path`: Ruta absoluta o relativa al archivo markdown. Si la ruta pasada es relativa, debe resolverse como relativa al directorio desde donde se invoca node - _currentworking directory_).
  `md-links <path-to-file> [options]`
  ![img_20180807_165632844](https://user-images.githubusercontent.com/32286663/43805090-1d60faee-9a64-11e8-974d-78f0382f7bc0.jpg)
 
--- index.js
+++ index.js
@@ -1,141 +1,49 @@
-const path = require('path');
-const fs = require('fs-extra');
+const { isAbsolute, resolve, join, extname, dirname, basename } = require('path')
+const { lstat, readFile, readdir } = require('fs-extra');
 const fetch = require('node-fetch');
 
 //Función que resuelve una ruta como absoluta
 const validatePath = (root) => {
-    if (path.isAbsolute(root)) {
-        console.log('Es una ruta absoluta');
-        console.log(path.isAbsolute());
+    if (isAbsolute(root)) {
+        return isAbsolute(root)
     } else {
-        console.log('Es una ruta relativa convertida a absoluta');
-        console.log(path.resolve());
+        return resolve(root)
     }
 };
-validatePath('readme.md');
 
-//Función que valida si es un archivo
-const validateIsFile = (file) => {
-    fs.lstat(file)
+//Función que valida si la ruta ingresada es un archivo o directorio
+const validateIsFileOrDirectory = (root) => {
+        lstat(root)
         .then(stat => {
-            console.log('Archivo ' + stat.isFile());
-            /*   stat.isFile(); //=> true / false */
+            if (stat.isFile()) { //verifica si es archivo
+                console.log(root);
+                return [root]
+            } else {
+                readdir(root) //Sino es carpeta, entonces la lee
+                    .then(files => {
+                        for (const file in files) {
+                            const element = files[file];
+                        }
+                        files.forEach(file => { //recorre cada carpeta para saber qué hay dentro
+                             const archivo = resolve(root + '/' + file);
+                             validateIsFileOrDirectory(archivo)
+                        })
+                    })
+             }
         })
         .catch(err => {
-            console.log(err);
+            console.log(`El archivo o file no existe ${err.path}`);
         });
 };
-validateIsFile('readme.md');
 
-//Función que valida si es un directorio
-const validateIsDirectory = (directory) => {
-    fs.lstat(directory)
-        .then(stat => {
-            console.log('Directorio ' + stat.isDirectory());
-            /*      stat.isDirectory(); //=> true / false */
-        })
-        .catch(err => {
-            console.log(err);
-        });
-};
-validateIsDirectory('C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory');
+/* return new Promise((resolve, reject) => {
 
-//Función que lee un archivo 
-const readFileContent = (path) => {
-    fs.readFile(path, 'utf8')
-        .then(content => {
-            console.log('Contenido File: ' + content);
-            /* console.log(path.extname('readme.md')); */
-            /*  console.log(path.extname(path)); */
-        })
-        .catch(err => {
-            console.log(err);
-        });
-};
-readFileContent('readme.md');
-
-//Función que verifica que sea un archivo markdown
-const checkFileMd = (nameFile) => {
-    const fileMd = /\.(md|mkdn|mdown|markdown?)$/i;
-    /*    return fileMd.test(path.extname(nameFile)); */
-    console.log(path.extname(nameFile));
-};
-checkFileMd('readme.md')
-
-//Función que lee un directorio(falta recursividad)
-const readDirectoryContent = (path) => {
-    fs.readdir(path)
-        .then(files => {
-            console.log('Contenido Directory:');
-            console.log(files);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-};
-readDirectoryContent('C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory');
-
-//Función que extrae los links de un archivo markdown
-/* const getLinksMd = (file) => {
-    let linksMd = [];
-    const textLink = /\[(.*)\]/gi;
-    const hrefLink = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&/=]*)/g;
-    //Agregar los links al array de objetos
-    linksMd.push({
-        text: textLink,
-        href: hrefLink,
-        file: path
-    });
-    console.log(linksMd);
-};
-getLinksMd('readme.md'); */
-
-//Ejemplito de Match
-var cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-var expresion = /[A-E]/gi;
-var array_emparejamientos = cadena.match(expresion);
-console.log(array_emparejamientos);
-
-/* Usando fetch para pedir archivo y consumir la respuesta(contenido)
-let promise = fetch('readme.md');
-promise.then(res => {
-    return res.json();
-}).then(json => {
-    console.log(json);
 }); */
 
-//Función para validar status de un link 
-const validateStatusLink = (link) => {
-    //ok
-    //fail
-};
+validateIsFileOrDirectory('C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory');
 
-//Función para validar el stats de los links
-const validateStats = (path) => {
-    //preguntar el estado!! usando node fetch para eso
-    //total
-    //unicos 
-    //rotos
-};
+/* validateIsFileOrDirectory('C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory')
+.then(result => {
+    console.log(result)
+}); */
 
-
-const mdLinks = (path, options) => {
-    return new Promise((resolve, reject) => {
-        if (!path) reject('Ingrese un archivo o directorio');
-        const pathAbs = path.resolve(path); //aqui resuelve las rutas que se ingresan como absolutas
-        /* console.log(path.resolve(path));
-        console.log(pathAbs);
-        console.log('antes de resolve arr') */
-        setTimeout(() => {
-            resolve([
-                {
-                    href: 'href',
-                    text: 'text',
-                    file: 'file'
-                }
-            ])
-        }, 1000)
-    })
-};
-
-module.exports = mdLinks; 
