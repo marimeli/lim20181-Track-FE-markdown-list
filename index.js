a30ff64fa35d7301ff4a1a4347556d67391096ff
marimeli <marimel.casas@gmail.com>
2018-09-14 16:47:31 -0500

    promises example

--- index.js
+++ index.js
@@ -33,17 +33,48 @@ const validateIsFileOrDirectory = (root) => {
         })
         .catch(err => {
             console.log(`El archivo o file no existe ${err.path}`);
+            return err;
         });
 };
-
-/* return new Promise((resolve, reject) => {
-
-}); */
 
 validateIsFileOrDirectory('C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory');
 
 /* validateIsFileOrDirectory('C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory')
 .then(result => {
     console.log(result)
-}); */
+});  */
 
+//Ejemplo de promesa
+
+function validateLink(arrayLinks) {
+    return arrayLinks.map(link => {
+        return fetch(link)
+        .then(res => {
+            return { status: res.status, text: "OK" }
+        })
+        .catch(e => {
+            return { status: "404", text: "Fail"}
+        })
+    })
+}
+
+const arrayLinks = [
+    "https://google.com",
+    "https://github.com"
+]
+
+function Hola() {
+    return new Promise((resolve, reject) => {
+        Promise.all(validateLink(arrayLinks))
+        .then(result => {
+            resolve(result)
+        })
+        .catch(e => {
+            reject(e)
+        })
+    })
+};
+
+Hola().then(res => {
+    console.log(res)
+});
