0f3ed52df388ff940f6f14acece1231cf7c0b1a2
marimeli <marimel.casas@gmail.com>
2018-09-15 01:04:02 -0500

    undefined

--- index.js
+++ index.js
@@ -2,55 +2,37 @@ const { isAbsolute, resolve, join, extname, dirname, basename } = require('path'
 const { lstat, readFile, readdir } = require('fs-extra');
 const fetch = require('node-fetch');
 
-//Función que resuelve una ruta como absoluta
-const validatePath = (root) => {
-    if (isAbsolute(root)) {
-        return isAbsolute(root)
-    } else {
-        return resolve(root)
-    }
-};
 
 //Función que valida si la ruta ingresada es un archivo o directorio. Debería retornar un array de archivos
-const validateIsFileOrDirectory = (path) => {
+const getFiles = (path) => {
     return lstat(path) // stat about a file
         .then(stat => {
-            let arrayLinks3 = []            
+            let arrayLinks3 = []
             if (stat.isFile()) { //verifica si es archivo
                 return [path]
             } else {
                 return readdir(path) //Sino es carpeta, entonces la lee
                     .then(files => {
-                        for (const file in files) {
-                            const element = files[file];
-                        }
-                        const arrayOfPromises = files.map(file => { //recorre cada carpeta para saber qué hay dentro
-                            const archivo = resolve(path,file);
-                            return validateIsFileOrDirectory(archivo)  
+                        const arrayOfPromises = files.map(file => {
+                            return getFiles(resolve(path, file))
                         })
                         Promise.all(arrayOfPromises)
                             .then(respuestas => {
                                 respuestas.forEach(res => {
                                     arrayLinks3 = arrayLinks3.concat(res)
-                                    console.log('concate', arrayLinks3)
+                                    console.log('concatenar', arrayLinks3)
                                 })
-                                // console.log('f', arrayLinks3);
-                                // return arrayLinks3;
+                                /* console.log('f', arrayLinks3);
+                                return arrayLinks3; */
                             })
                     })
             }
         })
-        // .catch(err => {
-        //     console.log(`El archivo o file no existe ${err.path}`);
-        //     return err;
-        // });
+        .catch(err => {
+            console.log(`El archivo o file no existe ${err.path}`);
+            return err;
+        });
 };
 
-validateIsFileOrDirectory(process.cwd() + '/test/directory')
-    .then(a => console.log('j',a))
-
-
-/* validateIsFileOrDirectory('C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory')
-.then(result => {
-    console.log(result)
-});  */
+getFiles(process.cwd() + '/test/directory')
+    .then(a => console.log('j', a))
