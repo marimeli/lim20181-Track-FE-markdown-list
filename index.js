173da8364c3fc58ea8ba8190045f07dcf0290c44
marimeli <marimel.casas@gmail.com>
2018-09-16 04:22:19 -0500

    closes #8

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-const { isAbsolute, resolve, join, extname, dirname, basename } = require('path')
+const { resolve, extname } = require('path')
 const { lstat, readFile, readdir } = require('fs-extra');
 const fetch = require('node-fetch');
 
+//Función que verifica que sea un archivo markdown
+const verifyIsMd = file => /\.(md|mkdn|mdown|markdown?)$/i.test(extname(file));
 
-//Función que valida si la ruta ingresada es un archivo o directorio. Debería retornar un array de archivos
-const getFiles = (path) => {
+//Función que valida si la ruta ingresada es un archivo o directorio y retorna un array de archivos
+const getArrFiles = (path) => {
     return lstat(path) // stat about a file
         .then(stat => {
-            let arrayLinks3 = []
             if (stat.isFile()) { //verifica si es archivo
-                return [path]
+                const fileMd = verifyIsMd(path);//llama a la función que verifica si la ruta es .md
+                if (fileMd) {
+                    return [path];
+                } else {
+                    return `El archivo no es markdown ${path}`
+                  }              
             } else {
-                return readdir(path) //Sino es carpeta, entonces la lee
+                return readdir(path) //Sino es archivo es carpeta, entonces la lee
                     .then(files => {
-                        const arrayOfPromises = files.map(file => {
-                            return getFiles(resolve(path, file))
+                        return files.map(file => {
+                            return getArrFiles(resolve(path, file));
                         })
-                        Promise.all(arrayOfPromises)
-                            .then(respuestas => {
-                                respuestas.forEach(res => {
-                                    arrayLinks3 = arrayLinks3.concat(res)
-                                    console.log('concatenar', arrayLinks3)
-                                })
-                                /* console.log('f', arrayLinks3);
-                                return arrayLinks3; */
-                            })
+                    })
+                    .then(arrayOfPromises => {
+                        return Promise.all(arrayOfPromises);
+                    })
+                    .then(files => {
+                        let arrayPath = [];
+                        files.forEach(file => {
+                            arrayPath = arrayPath.concat(file);
+                        })
+                        return arrayPath;
                     })
             }
         })
@@ -34,5 +41,68 @@ const getFiles = (path) => {
         });
 };
 
-getFiles(process.cwd() + '/test/directory')
-    .then(a => console.log('j', a))
+
+getArrFiles(process.cwd() + '/test/directory')
+    .then(arrFiles => console.log('array de files', arrFiles))
+
+//Función que lee un archivo markdown y extrae los links. 
+//Debería retornar un array de links
+const readFileContent = (arrMd) => {
+    readFile(arrMd, 'utf8')
+        .then(content => {
+            console.log('Contenido File: ' + content);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+ };
+ readFileContent('readme.md');
+
+//Función que recibe array de links y sireve para validar status de un link
+//Devuelve el status de cada link
+const validateStatusLink = (link) => {
+    //ok
+    //fail
+ };
+ 
+ //Función para validar el stats de los links
+ const validateStats = (path) => {
+    //preguntar el estado!! usando node fetch para eso
+    //total
+    //unicos
+    //rotos
+ };
+
+ //Ejercicio de status de links con promesas
+const arrayLinks = [
+    "https://google.com",
+    "https://github.com"
+]
+
+const validateLink = (arrayLinks) => {
+    return arrayLinks.map(link => {
+        return fetch(link)
+        .then(res => {
+            return { status: res.status, text: "OK" }
+        })
+        .catch(e => {
+            return { status: "404", text: "Fail"}
+        })
+    })
+};
+
+const Hola = () => {
+    return new Promise((resolve, reject) => {
+        Promise.all(validateLink(arrayLinks))
+        .then(result => {
+            resolve(result)
+        })
+        .catch(e => {
+            reject(e)
+        })
+    })
+}
+
+Hola().then(res => {
+    console.log(res)
+})
