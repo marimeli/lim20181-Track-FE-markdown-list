a3a7ad6356d51776cc8a6587d01ba5edcd83b4ea
marimeli <marimel.casas@gmail.com>
2018-09-18 18:10:02 -0500

    corrige funcionalidad filter

--- cli.js
+++ cli.js
@@ -3,5 +3,8 @@
 const [,, ...argvs] = process.argv;
 const totalArgvs = argvs.length;
 const path = argvs[0];
+console.log( argvs);
+console.log(path);
+
 const mdLinks = require('./index');
 
--- index.js
+++ index.js
@@ -5,61 +5,62 @@ const fetch = require('node-fetch');
 
 //Función que valida si la ruta ingresada es un archivo o directorio y retorna un array de archivos
 const getArrFiles = (path) => {
-    return lstat(path) // stat about a file
-        .then(stat => {
-            if (stat.isFile()) { //verifica si es archivo
-                return [path];
-            } else {
-                return readdir(path) //Sino es archivo es carpeta, entonces la lee
-                    .then(files => {
-                        return files.map(file => {
-                            return getArrFiles(resolve(path, file));
-                        })
-                    })
-                    .then(arrayOfPromises => {
-                        return Promise.all(arrayOfPromises);
-                    })
-                    .then(files => {
-                        let arrayPath = [];
-                        files.forEach(file => {
-                            arrayPath = arrayPath.concat(file);
-                        })
-                        return arrayPath;
-                    })
-            }
-        })
-        .catch(err => {
-            console.log(`El archivo o file no existe ${err.path}`);
-            return err;
-        });
+  return lstat(path) // stat about a file
+    .then(stat => {
+      if (stat.isFile()) { //verifica si es archivo
+        return [path];
+      } else {
+        return readdir(path) //Sino es archivo es carpeta, entonces la lee
+          .then(files => {
+            return files.map(file => {
+              return getArrFiles(resolve(path, file));
+            })
+          })
+          .then(arrayOfPromises => {
+            return Promise.all(arrayOfPromises);
+          })
+          .then(files => {
+            let arrayPath = [];
+            files.forEach(file => {
+              arrayPath = arrayPath.concat(file);
+            })
+            return arrayPath;
+          })
+      }
+    })
+    .catch(err => {
+      return `El archivo o file no existe ${err.path}`;
+    });
 };
 
-//Función que verifica que sea un archivo markdown
-const verifyIsMd = file => /\.(md|mkdn|mdown|markdown?)$/i.test(extname(file));
+//Función que verifica que sea un archivo markdown y los filtra
+const verifyIsMd = (arrFiles) => new Promise((resolve, reject) => {
+  resolve(arrFiles.filter(file => /\.(md|mkdn|mdown|markdown?)$/i.test(extname(file))))
+})
 
 //Función lee y que extrae los links de un archivo markdown. Retorna un array de links
 const getLinksMd = (arrayFiles) => {
-    return new Promise((resolve, reject) => {
-        const links = [];
-        arrayFiles.forEach(file => {
-            const fileMd = readFileSync(file, 'utf8');
-            const renderer = new marked.Renderer();
-            renderer.link = (href, title, text) => {
-                links.push({
-                    href: href,
-                    text: text,
-                    file: file
-                });
-            };
-            marked(fileMd, { renderer })
-        })
-        resolve(links);
+  return new Promise((resolve, reject) => {
+    const links = [];
+    arrayFiles.forEach(file => {
+      const fileMd = readFileSync(file, 'utf8');
+      const renderer = new marked.Renderer();
+      renderer.link = (href, title, text) => {
+        links.push({
+          href: href,
+          text: text,
+          file: file
+        });
+      };
+      marked(fileMd, { renderer })
     })
+    resolve(links);
+  })
 };
 
 //Función que recibe array de links y retorna un nuevo array con solo el contenido href 
 //Usando fetch para pedir archivo y consumir la respuesta(contenido)
-const validateLink = (arrayLinks) => {
+const validateLinks = (arrayLinks) => {
   const arrLinks = arrayLinks.map(objLink => fetch(objLink.href))
   return Promise.all(arrLinks)
     .then(response => {
@@ -70,34 +71,31 @@ const validateLink = (arrayLinks) => {
       });
       return links;
     })
- };
+};
 
 //Función para validar el stats de los links
 const validateStats = (path) => {
-    //preguntar el estado!! usando node fetch para eso
-    //total
-    //unicos
-    //rotos
+  //preguntar el estado!! usando node fetch para eso
+  //total
+  //unicos
+  //rotos
+};
+const mdLinks = (path, options) => {
+  //console.log('path', path);
+  //if (!path) reject('Ingrese un archivo o directorio');
+  getArrFiles(resolve(path))// Me va a indicar donde se está ejecutando el archivo
+    .then(verifyIsMd)
+    .then(getLinksMd)
+    .then(result => !options ? result : validateLinks(result))
+    .then(o => {
+      console.log('hola o', o);
+    })
+  /* return new Promise((resolve, reject) => {
+  }) */
 };
 
-getArrFiles(process.cwd() + '/test/directory') // Me va a indicar donde se está ejecutando el archivo
-    .then(arrFiles => arrFiles.filter(verifyIsMd))
-    .then(getLinksMd)
-    .then(validateLink)
-    .then(console.log)
+//mdLinks('readme.md')
+mdLinks((process.cwd() + '//test//directory'), '')
 
+module.exports = mdLinks;
 
- /*    const mdLinks = (path, options) => {
-      return new Promise((resolve, reject) => {
-          if (!path) reject('Ingrese un archivo o directorio');
-          const arrFilesMd = getArrFiles(resolve(path)) //aqui resuelve las rutas que se ingresan como absolutas
-          if (arrFilesMd.length === 0) {
-              resolve('Tu archivo o carpeta no tiene links');
-          }
-      })
-  };
-  
-  module.exports = mdLinks;
-   */
-    
-    
