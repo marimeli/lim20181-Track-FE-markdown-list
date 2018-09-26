3af479c541536b343326e7ea78e3dbf3f33d6788
marimeli <marimel.casas@gmail.com>
2018-09-26 18:42:00 -0500

    new links

--- test/directory/dir1/readme2.md
+++ test/directory/dir1/readme2.md
@@ -1,5 +1,6 @@
 Hey! 
 
 [Google](https://www.google.com.pe)
-[Google2](https://www.google.com.pe)
-[Google3](https://www.gle.com.pe)
+[Algo](http://algo.com/2/3/)
+[Node.js](https://nodejs.org/es/)
+[Google.caido](https://www.gle.com.pe)
--- test/index.test.js
+++ test/index.test.js
@@ -15,6 +15,36 @@ describe('mdLinks', () => {
     return expect(mdLinks(process.cwd() + '//test//directory', options)).toBeInstanceOf(Promise);
   });
 
+  test('debería retornar un array de objetos con la propiedad href, text y path', () => {
+    return mdLinks(process.cwd() + '//test//directory', options)
+      .then(arrayLinks => {
+        expect(arrayLinks).toEqual([{
+          href: 'https://es.wikipedia.org/wiki/Markdown',
+          text: 'Markdown',
+          file: 'C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory\\dir1\\readme.md'
+        },
+        {
+          href: 'https://www.google.com.pe',
+          text: 'Google',
+          file: 'C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory\\dir1\\readme2.md'
+        },
+        {
+          href: 'http://algo.com/2/3/',
+          text: 'Algo',
+          file: 'C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory\\dir1\\readme2.md'
+        },
+        {
+          href: 'https://nodejs.org/es/',
+          text: 'Node.js',
+          file: 'C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory\\dir1\\readme2.md'
+        },
+        {
+          href: 'https://www.gle.com.pe',
+          text: 'Google.caido',
+          file: 'C:\\Users\\Melissa Casas\\Documents\\markdown\\lim20181-Track-FE-markdown-list\\test\\directory\\dir1\\readme2.md'
+        }])
+      })
+  })
 
 });
 
